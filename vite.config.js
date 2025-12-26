39887ea217207fd16d81ba423c2ad69ffa18dd3b
TIaomiao <xvfengnianwzj@gmail.com>
2025-12-26 22:52:44 +0800

    Update vite.config.js
    改一下 加一个unocss

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: '/mnemo-nbackforzianself/', // 这里必须对应你的仓库名
+  plugins: [
+    vue(),
+    UnoCSS(), // 这里的 UnoCSS 插件是项目美化样式的核心，必须开启
+  ],
+  // 这里的路径必须和你的仓库名 mnemo-nbackforzianself 一致
+  base: '/mnemo-nbackforzianself/', 
 })
