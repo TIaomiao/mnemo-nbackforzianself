962f7ea2e070a45fd148fba5dbaf17790363fc9f
TIaomiao <xvfengnianwzj@gmail.com>
2025-12-26 22:12:19 +0800

    Update vite.config.js
    by Gemini

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'./', //自用修改,Gemini教我的
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-    },
-  },
-  build: {
-    rollupOptions: {
-      // 避免使用 native bindings
-      treeshake: true,
-    },
-  },
+  base: '/mnemo-nbackforzianself/', // 这里必须对应你的仓库名
 })
