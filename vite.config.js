cdfc117a82d6ee103a5943778de54f75e233054c
TIaomiao <xvfengnianwzj@gmail.com>
2025-12-26 23:29:04 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    UnoCSS(), // 开启 UnoCSS 插件，解决之前 image_bd7a0d.jpg 里的报错
+    UnoCSS(), 
   ],
-  // 必须确保 base 路径和你现在的仓库名 mnemo-nbackforzianself 一致
   base: '/mnemo-nbackforzianself/', 
 })
