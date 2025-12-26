c633f6f9602e9b1242f96355341bcf84c40017ad
TIaomiao <xvfengnianwzj@gmail.com>
2025-12-26 23:03:59 +0800

    Update vite.config.js
    我也不知道我之前的和这个除了备注有啥区别

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    UnoCSS(), // 这里的 UnoCSS 插件是项目美化样式的核心，必须开启
+    UnoCSS(), 
   ],
-  // 这里的路径必须和你的仓库名 mnemo-nbackforzianself 一致
+  // 必须确保 base 路径和你现在的仓库名完全一致
   base: '/mnemo-nbackforzianself/', 
 })
